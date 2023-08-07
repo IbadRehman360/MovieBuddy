34df188eb3d75e8b6a4c8754d40c3de139dd9db2
IbadRehman360 <ibadhashim4@gmail.com>
2023-08-07 20:44:40 +0500

    [Project Name] - Added initial movie site structure and implemented extensive logic
    This commit introduces the initial structure of the movie site, including the necessary HTML, CSS, and JavaScript files. Additionally, a significant amount of logic has been written to handle various functionalities of the site, such as displaying movie information, handling user interactions, and managing dynamic content.
    
    Changes made in this commit:
    - Created index.html with basic layout and placeholders for movie content.
    - Set up the main styles.css file to apply a responsive design and improve the overall appearance.
    - Implemented JavaScript logic to fetch movie data from an external API and dynamically display it on the site.
    - Created functions to handle user interactions, like sorting movies by different criteria and filtering based on genres.
    - Added error handling and data validation to ensure a smooth user experience.
    - Set up a navigation bar and footer to enhance site navigation and user interaction.
    - Organized code into modular functions and reusable components to improve maintainability and readability.
    
    Please review the changes and provide feedback for further improvements.
    
    [#issue_number] [optional hashtag related to a specific issue or feature]

--- src/Components/App.js
+++ src/Components/App.js
@@ -118,7 +118,7 @@ function Nav({ children }) {
         <nav className="nav-bar">
             <div className="logo">
                 <span role="img">🍿</span>
-                <h1>usePopcorn</h1>
+                <h1>MovieBuddy </h1>
             </div>
             {children}
         </nav>
